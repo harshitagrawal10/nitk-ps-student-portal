b82531d4b27feef55145d2938263cb78191cd33a
harshitagrawal10 <harshitagrawal10@gmail.com>
2018-03-24 22:29:48 +0530

    24-03-2018

---
+++ js/image.js
@@ -0,0 +1,42 @@
+
+$(function () {
+    $("#upload").bind("click", function () {
+        //Get reference of FileUpload.
+        var fileUpload = $("#fileUpload")[0];
+ 
+        //Check whether the file is valid Image.
+        var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(.jpg|.png|.gif)$");
+        if (regex.test(fileUpload.value.toLowerCase())) {
+            //Check whether HTML5 is supported.
+            if (typeof (fileUpload.files) != "undefined") {
+                //Initiate the FileReader object.
+                var reader = new FileReader();
+                //Read the contents of Image File.
+                reader.readAsDataURL(fileUpload.files[0]);
+                reader.onload = function (e) {
+                    //Initiate the JavaScript Image object.
+                    var image = new Image();
+                    //Set the Base64 string return from FileReader as source.
+                    image.src = e.target.result;
+                    image.onload = function () {
+                        //Determine the Height and Width.
+                        var height = this.height;
+                        var width = this.width;
+                        if (height > 200 || width > 200) {
+                            alert("Height and Width must not exceed 200px.");
+                            return false;
+                        }
+                        //alert("Uploaded image has valid Height and Width.");
+                        return true;
+                    };
+                }
+            } else {
+                alert("This browser does not support HTML5.");
+                return false;
+            }
+        } else {
+            alert("Please select a valid Image file.");
+            return false;
+        }
+    });
+});
